1bba7ce8d45d1aad22ef97b0f1dab8619d474c64
vitorxfs <viisanches98@gmail.com>
2018-05-25 17:51:59 -0300

    Set deploying to heroku

--- server.js
+++ server.js
@@ -1,32 +1,9 @@
-// const express= require('express');
-// const path = require('path');
-// const http = require('http').Server(express);
-// const io = require('socket.io')(http);
-//
-// const port = 3000;
-//
-// app = express();
-//
-// app.use(express.static(path.join(__dirname, '')));
-//
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + '/index.html');
-// });
-//
-// io.on('connection', (socket) => {
-//   console.log('user connected');
-// });
-//
-// http.listen(port, ()=>{
-//   console.log('Server listening on', port);
-// });
-
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/pg'));
 
